09be92ef34c483932086785b1840fef802652f48
Stanlys <stanlys@yandex.ru>
2022-09-21 21:51:02 +0300

    feat: install swagger

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post, Controller, Get} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  constructor(private userService:UsersService) {
+
+  }
+  
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this
+  }
+
+  @Get()
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+}
